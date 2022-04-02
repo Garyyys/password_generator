50caf8fb9b5f86504f7701095288013e6978e485
Dima <Allgarryyys@gmail.com>
2022-04-02 17:44:47 +0300

    Added 2 pages. Bootstrap templates. Buttoms to return to homepage

--- generator/views.py
+++ generator/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+import random
+import string
 
 # Create your views here.
 def home(request):
@@ -7,4 +9,25 @@ def home(request):
 
 
 def password(request):
-    return render(request, 'generator/password.html')
+
+    characters = list(string.ascii_lowercase)
+    lenght = int(request.GET.get('lenght', 12))
+    thepassword = ''
+
+    if request.GET.get('uppercase'):
+        characters.extend(list('ABCDEFGHIJKLMNOPQRSTUVWXYZ'))
+    if request.GET.get('special'):
+        characters.extend(list('!@#$%^&*()_+'))
+    if request.GET.get('numbers'):
+        characters.extend(list(string.digits))
+
+    for x in range(lenght):
+        thepassword += random.choice(characters)
+
+    return render(request, 'generator/password.html', {'password': thepassword})
+
+def again_generate():
+    pass
+
+def description(request):
+    return render(request, 'generator/description.html')
--- password_generation/urls.py
+++ password_generation/urls.py
@@ -17,6 +17,7 @@ from django.urls import path
 from generator import views
 
 urlpatterns = [
-    path('', views.home),
+    path('', views.home, name='home'),
+    path('description/', views.description, name='description'),
     path('generatedpassword/', views.password, name='password'),
 ]
